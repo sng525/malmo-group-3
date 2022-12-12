0a44152898f4e0adab6ab5489d9b7b952acd486a
carolinenorback <109136667+carolinenorback@users.noreply.github.com>
2022-12-12 14:11:37 +0100

    Revert "changes id to 0 in MainActivity"
    This reverts commit 4da83663d6fb6d460aa9625e61fb5039595f99f5.

--- app/src/main/java/se/vejbystrand/ourapplication/MainActivity.java
+++ app/src/main/java/se/vejbystrand/ourapplication/MainActivity.java
@@ -34,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
 
         btn_add.setOnClickListener(view -> {
             try {
-                exercise = new Exercise(0,
+                exercise = new Exercise(-1,
                         activity.getText().toString(),
                         Integer.parseInt(weight.getText().toString()),
                         Integer.parseInt(set.getText().toString()),
