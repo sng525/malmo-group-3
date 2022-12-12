4c3328e3a4f8bc81a96a25b9f4cd409b7104e187
Jiaojiao Song <ssssjulia525@gmail.com>
2022-12-12 14:26:50 +0100

    the lastest one

--- app/src/main/java/se/vejbystrand/ourapplication/DatabaseHelper.java
+++ app/src/main/java/se/vejbystrand/ourapplication/DatabaseHelper.java
@@ -6,13 +6,9 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.List;
-import android.content.ContentValues;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
 
@@ -24,7 +20,13 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String COLUMN_SETS = "COLUMN_SETS";
     public static final String COLUMN_REPS = "COLUMN_REPS";
     public static final String COLUMN_DATE = "COLUMN_DATE";
-    SQLiteOpenHelper sqlite;
+
+    private static DatabaseHelper instance = null;
+    public static DatabaseHelper getInstance(Context context){
+        if (instance == null)
+            instance = new DatabaseHelper(context.getApplicationContext());
+        return instance;
+    }
 
     //CONSTRUCTOR
     public DatabaseHelper(@Nullable Context context) {
@@ -35,16 +37,17 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         String createTableStatement = "CREATE TABLE " + WORKOUTS_TABLE + " (" + COLUMN_ID
-                + " STRING PRIMARY KEY AUTOINCREMENT, "
-                + COLUMN_ACTIVITY + " TEXT, " + COLUMN_WEIGHT + " STRING, " + COLUMN_SETS
-                + " STRING, " + COLUMN_REPS + " STRING, " + COLUMN_DATE + " TEXT)";
+                + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + COLUMN_ACTIVITY + " TEXT," + COLUMN_WEIGHT + " INTEGER," + COLUMN_SETS
+                + " INTEGER," + COLUMN_REPS + " INTEGER," + COLUMN_DATE + " TEXT)";
 
         db.execSQL(createTableStatement);
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-
+    public void onUpgrade(SQLiteDatabase db, int oldVers, int newvers) {
+        db.execSQL("DROP TABLE IF EXISTS " + WORKOUTS_TABLE);
+        onCreate(db);
     }
 
     public boolean addWorkout(Exercise exercise) {
@@ -67,31 +70,6 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         }
     }
 
-    public void save(Exercise exercise) {
-        if(exercise.getId() == 0)   // if the id is 0, the book does not exist
-            insertWorkout(exercise);   // therefore insert it
-
-    }
-
-    private void insertWorkout(Exercise exercise) {
-
-        SQLiteDatabase db = sqlite.getWritableDatabase();
-        ContentValues c = getContentValues(exercise);
-        db.insert(WORKOUTS_TABLE, null, c);
-    }
-
-    @NonNull
-    private ContentValues getContentValues(Exercise exercise) {
-        // Creates the column-value pairs given a book
-        ContentValues c = new ContentValues();
-        c.put("activity", exercise.getActivity());
-        c.put("weight", exercise.getWeight());
-        c.put("set", exercise.getSet());
-        c.put("reps", exercise.getReps());
-        c.put("date", exercise.getDate());
-
-        return c;
-    }
 //CREATE METHODS TO PULL ALL ITEMS OUT OF THE DATABASE
 
 //DEFINE THE PROPERTIES THAT WE WANT THE LIST TO RETURN
@@ -99,7 +77,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public ArrayList<Exercise> selectAll() {
         ArrayList<Exercise> returnList = new ArrayList<>();
 
-        String queryString = "SELECT * FROM WORKOUTS_TABLE" + WORKOUTS_TABLE;
+        String queryString = "SELECT * FROM " + WORKOUTS_TABLE;
         SQLiteDatabase db = this.getReadableDatabase();
         Cursor cursor = db.rawQuery(queryString, null);                   //EXECUTES QUERY
 
@@ -113,16 +91,37 @@ public class DatabaseHelper extends SQLiteOpenHelper {
                 String date = cursor.getString(5);
 
                 Exercise newWorkout = new Exercise(id, activity, weights, sets, reps, date);
+                returnList.add(newWorkout);
 
-
-            } while (cursor.moveToFirst());
-
-            cursor.close();
+            } while (cursor.moveToNext());
         }
+        db.close();
+        cursor.close();
+
         return returnList;
     }
 
+    public void save(Exercise exercise) {
+        if(exercise.getId() == 0)   // if the id is 0, the book does not exist
+            insertWorkOut(exercise);   // therefore insert it
+    }
 
+    private void insertWorkOut(Exercise exercise){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues c = getContentValues(exercise);
+        db.insert(WORKOUTS_TABLE, null, c);
+    }
+
+    private ContentValues getContentValues(Exercise exercise) {
+        // Creates the column-value pairs given a book
+        ContentValues c = new ContentValues();
+
+        c.put(COLUMN_ACTIVITY, Exercise.getActivity());
+        c.put(COLUMN_WEIGHT, Exercise.getWeight());
+        c.put(COLUMN_SETS, Exercise.getSet());
+        c.put(COLUMN_REPS, Exercise.getReps());
+        c.put(COLUMN_DATE, Exercise.getDate());
+
+        return c;
+    }
 }
-
-
--- app/src/main/java/se/vejbystrand/ourapplication/ViewAll.java
+++ app/src/main/java/se/vejbystrand/ourapplication/ViewAll.java
@@ -3,14 +3,28 @@ package se.vejbystrand.ourapplication;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-    public class ViewAll extends AppCompatActivity {
+public class ViewAll extends AppCompatActivity {
+
+    DatabaseHelper db;
+    RecyclerView workoutListView;
+    ExerciseAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.recy_view);
 
-    }
+        db = DatabaseHelper.getInstance(getApplicationContext());
+
+        // Binding the Adapter to the RecyclerView
+        workoutListView = findViewById(R.id.workout_list_view);
+        adapter = new ExerciseAdapter(ViewAll.this, db.selectAll());
+        workoutListView.setAdapter(adapter);
+        workoutListView.setLayoutManager(new LinearLayoutManager(this));
 
     }
+
+}
