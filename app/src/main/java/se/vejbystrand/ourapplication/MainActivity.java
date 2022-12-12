418e47fd978e3c94834a837cf240079658437188
carolinenorback <109136667+carolinenorback@users.noreply.github.com>
2022-12-12 14:13:45 +0100

    Revert "fix the error - button plus click"
    This reverts commit d9b53063006b4da50d155d192e1a1c9a8af1e00f.

--- app/src/main/java/se/vejbystrand/ourapplication/MainActivity.java
+++ app/src/main/java/se/vejbystrand/ourapplication/MainActivity.java
@@ -1,22 +1,29 @@
 package se.vejbystrand.ourapplication;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-
 public class MainActivity extends AppCompatActivity {
 
-    EditText activity, weight, set, reps, date;
+    EditText activity;
+    EditText weight;
+    EditText set;
+    EditText reps;
+    EditText date;
+    Button btn_add;
     DatabaseHelper db;
-    Exercise exercise;
-    FloatingActionButton btn_add;
+    RecyclerView workoutListView;
+    ExerciseAdapter adapter;
+
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -30,27 +37,43 @@ public class MainActivity extends AppCompatActivity {
         reps = findViewById(R.id.num_reps);
         date = findViewById(R.id.date_time);
 
-        btn_add = findViewById(R.id.btn_add);
+       // onBtnAddClick();
 
-        btn_add.setOnClickListener(view -> {
-            try {
-                exercise = new Exercise(-1,
-                        activity.getText().toString(),
-                        Integer.parseInt(weight.getText().toString()),
-                        Integer.parseInt(set.getText().toString()),
-                        Integer.parseInt(reps.getText().toString()),
-                        date.getText().toString());
-                Toast.makeText(MainActivity.this, "Successfully added", Toast.LENGTH_SHORT).show();
-            } catch (Exception e) {
-                exercise = new Exercise(-1, "error", 0, 0, 0, "error");
-                Toast.makeText(MainActivity.this, "Error creating workout", Toast.LENGTH_SHORT).show();
-            }
+        /*workoutListView = findViewById(R.id.workout_list_view);
+        db = new DatabaseHelper(MainActivity.this);
 
-            db = DatabaseHelper.getInstance(getApplicationContext());
-            db.save(exercise);
-    });
+        adapter = new ExerciseAdapter(MainActivity.this, db.selectAll());
+
+        workoutListView.setAdapter(adapter);
+        workoutListView.setLayoutManager(new LinearLayoutManager(this));*/
+
     }
+/*
+    private void onBtnAddClick() {
+        btn_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Exercise exercise;
+                try {
+                    exercise = new Exercise(-1,
+                            activity.getText().toString(),
+                            Integer.parseInt(weight.getText().toString()),
+                            Integer.parseInt(set.getText().toString()),
+                            Integer.parseInt(reps.getText().toString()),
+                            date.toString());
+                    Toast.makeText(MainActivity.this, "Successfully added", Toast.LENGTH_SHORT).show();
+                } catch (Exception e) {
+                    exercise = new Exercise(-1, "error", 0, 0, 0, "error");
+                    Toast.makeText(MainActivity.this, "Write correct in the lines", Toast.LENGTH_SHORT).show();
+                }
 
+                db = new DatabaseHelper(MainActivity.this);
+                db.addWorkout(exercise);
+
+            }
+        });
+    }
+*/
     public void onBtnViewClick(View view) {
         Intent intent = new Intent(this, ViewAll.class);
         startActivity(intent);
--- app/src/main/java/se/vejbystrand/ourapplication/ViewAll.java
+++ app/src/main/java/se/vejbystrand/ourapplication/ViewAll.java
@@ -3,28 +3,14 @@ package se.vejbystrand.ourapplication;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
-public class ViewAll extends AppCompatActivity {
-
-    DatabaseHelper db;
-    RecyclerView workoutListView;
-    ExerciseAdapter adapter;
+    public class ViewAll extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.recy_view);
 
-        db = DatabaseHelper.getInstance(getApplicationContext());
-
-        // Binding the Adapter to the RecyclerView
-        workoutListView = findViewById(R.id.workout_list_view);
-        adapter = new ExerciseAdapter(ViewAll.this, db.selectAll());
-        workoutListView.setAdapter(adapter);
-        workoutListView.setLayoutManager(new LinearLayoutManager(this));
-
     }
 
-}
+    }
